b2203516446a95b1d635fa4a8edbbe01efc9903d
Yavuzhn <61223169+Yavuzhanzgen@users.noreply.github.com>
2025-07-13 11:31:28 +0300

    Update ResourceIsimXor.py

--- ResourceIsimXor.py
+++ ResourceIsimXor.py
@@ -1,1 +1,12 @@
+def smethod_1(int_0):
+  
+    array = list("\u001e\u0001\u0017\u0013") // dll içerisindeki sabit
+    
+    for i in range(len(array)):
+        array[i] = chr((ord(array[i]) ^ ord('d')) ^ int_0) //yapilan islem
+    
+    return ''.join(array)
 
+print(smethod_1(0)) 
+
+//amac resource içindeki dosyanın ismini çekme bu dosya 2 katmanlı 
